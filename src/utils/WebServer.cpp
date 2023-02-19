5ab94597342d59f10066b5590adb146dfc4371be
KitsuneYokai <56147653+KitsuneYokai@users.noreply.github.com>
2023-02-19 19:14:11 +0100

    added Root Endpoint
    added the root endpoint to return some stuff, like the version of the KitsuDeck firmware (have to find out how to use .env), if its protected by a password(needed to add it to the dash, for future ws requests).
    
    I will have to figure out how i will store the makros on the keyboard. I will probably look if i can use sqlite or some sort of database.

--- src/utils/WebServer.cpp
+++ src/utils/WebServer.cpp
@@ -8,7 +8,7 @@
 #include "Settings.h"
 
 #define SERVER_PORT 80
-#define HELLO_ENDPOINT "/hello"
+#define ROOT_ENDPOINT "/"
 
 AsyncWebServer server(SERVER_PORT);
 AsyncWebSocket webSocket("/ws");
@@ -22,6 +22,27 @@ void handleWebSocketMessage(AsyncWebSocket *server, AsyncWebSocketClient *client
     }
 }
 
+// Handle HTTP GET request for /
+void handleRootRequest(AsyncWebServerRequest *request)
+{
+    // make a json
+    StaticJsonDocument<200> doc;
+    doc["status"] = true;
+    doc["message"] = "Hi, Im KitsuDeck, nice to meet you! Im a Makro keyboard thats connected to some pc in your Network, and i can do some other cool stuff, so please dont delete me from your Network, because someone in your network is using me!";
+    doc["version"] = "0.0.1";
+    // if any auth variable in the settings is set, let it be only the username, set the json protected value to true
+    if (getSettings("computer_user") != "" || getSettings("computer_password") != "")
+    {
+        doc["protected"] = true;
+    }
+    else
+    {
+        doc["protected"] = false;
+    }
+
+    request->send(200, "text/plain", doc.as<String>());
+}
+
 // Start the web server and WebSocket server
 void startWebServer(void *parameter)
 {
@@ -33,9 +54,8 @@ void startWebServer(void *parameter)
         // Connect to WiFi
         connectToWifi(getSettings("wifi_ssid"), getSettings("wifi_password"));
 
-        // Handle HTTP GET request for /hello
-        server.on(HELLO_ENDPOINT, HTTP_GET, [](AsyncWebServerRequest *request)
-                  { request->send(200, "text/plain", "Hello World"); });
+        // Setup HTTP GET handler for /
+        server.on(ROOT_ENDPOINT, HTTP_GET, handleRootRequest);
 
         // Setup WebSocket server
         webSocket.onEvent(handleWebSocketMessage);
